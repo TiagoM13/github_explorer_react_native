98e04048e736f204d2d9d98a61413668fb66ca7e
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-11 13:00:39 -0300

    feat: add safe ara context

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,18 @@
 import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Header } from '../components/Header/Header';
 import { RepositoryList } from '../components/RepositoryList/RepositoryList';
 
+import { styles } from './styles';
+
 const Home = () => {
   return (
-    <View>
-      <Header />
-      <RepositoryList />
+    <View style={styles.container}>
+      <SafeAreaView>
+        <Header />
+        <RepositoryList />
+      </SafeAreaView>
     </View>
   );
 }
