be452a9b2834894c7fa946c39b025ccb894760a6
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-11 09:56:17 -0300

    feat: add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Repository List</Text>
+    </View>
+  );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ccc',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginBottom: 16
+  },
+  text: {
+    fontSize: 18,
+    fontFamily: 'Inter_900Black',
+  }
+});
