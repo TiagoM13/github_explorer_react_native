fc0647093c09cf41596de634193d41d9c22553a3
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-11 21:53:09 -0300

    adding repository url

--- src/components/RepositoryItem/RepositoryItem.tsx
+++ src/components/RepositoryItem/RepositoryItem.tsx
@@ -1,18 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Linking } from "react-native";
 
-import { IRespositoryItem } from '@src/interfaces/repositoryItem';
+import { IRespositoryItem } from "@src/interfaces/repositoryItem";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 export const RepositoryItem = ({ repository }: IRespositoryItem) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        {repository.name}
-      </Text>
-      <Text style={styles.description}>
-        {repository.description}
-      </Text>
+      <Text style={styles.text}>{repository.name}</Text>
+      <Text style={styles.description}>{repository.description}</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => Linking.openURL(repository.html_url)}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.textButton}>Acessar repo</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
