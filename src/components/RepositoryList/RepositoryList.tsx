737cb26c5fe4df2c7704c9a2bbd9953a8e856f3e
TiagoM13 <thyagomotha2000@gmail.com>
2023-05-07 14:59:22 -0300

    adding ternary condition

--- src/components/RepositoryList/RepositoryList.tsx
+++ src/components/RepositoryList/RepositoryList.tsx
@@ -10,25 +10,23 @@ import { styles } from './styles';
 export const RepositoryList = () => {
   const { data, isLoading } = useQuery('repos', getAllRepositories);
 
-  if (isLoading) {
-    return (
-      <View style={styles.loading}>
-        <Text style={styles.textLoad}>Loading repositories...</Text>
-      </View>
-    );
-  }
-
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Lista de Repositórios</Text>
 
-      <FlatList
-        data={data}
-        keyExtractor={item => item.name}
-        renderItem={({ item }) => <RepositoryItem repository={item} />}
-        contentContainerStyle={styles.contentStyle}
-        showsVerticalScrollIndicator={false}
-      />
+      {isLoading ? (
+        <View style={styles.loading}>
+          <Text style={styles.textLoad}>Loading repositories...</Text>
+        </View>
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={item => item.name}
+          renderItem={({ item }) => <RepositoryItem repository={item} />}
+          contentContainerStyle={styles.contentStyle}
+          showsVerticalScrollIndicator={false}
+        />
+      )}
     </View>
   );
 };
